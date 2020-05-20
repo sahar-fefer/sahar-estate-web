ec975204177bf875290cf483d1f09d08f5969b95
saharovich <55681558+saharovich@users.noreply.github.com>
2020-05-20 12:21:15 +0300

    responsive footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// Icons
+import {
+    FaFacebookSquare,
+    FaInstagramSquare,
+    FaTwitterSquare,
+    FaLinkedin,
+    FaPinterestSquare
+} from 'react-icons/fa';
+
+const Footer = props => {
+    const logos = [{ icon: 'FaFacebookSquare', name: 'facebook' },
+    { icon: 'FaTwitterSquare', name: 'twitter' },
+    { icon: 'FaInstagramSquare', name: 'instagram' },
+    { icon: 'FaLinkedin', name: 'linkedin' },
+    { icon: 'FaPinterestSquare', name: 'pinterest' }]
+    return (
+        <div id="footer">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="about-me col-sm-6">
+                    © made by Sahar Feferovich 2020
+                    </div>
+                    <div className="icons-wrapper col-sm-6 flex-sm-nowrap">
+                        {/* {
+                            logos.map(logo => {
+                                return (
+                                <div>
+                                    <a href={`https://www.${logo.name}.com`} target="_blank">
+                                        <logo.icon className={`footer-icon ${logo.name}`} />
+                                        {logo.name === 'instagram' && <logo.icon className={`footer-icon ${logo.name}-hover`} />}
+                                    </a>
+                                </div>
+                            )})
+                        } */}
+                        <div><a href="https://www.facebook.com" target="_blank"><FaFacebookSquare className='footer-icon facebook' /></a>
+                        </div>
+                        <div><a href="https://www.twitter.com" target="_blank"><FaTwitterSquare className={"footer-icon twitter"} /></a></div>
+                        <div><a href="https://www.linkedin.com" target="_blank"><FaLinkedin className={"footer-icon linkedin"} /></a></div>
+                        <div className={"instagram-wrapper"}>
+                            <a href="https://www.instagram.com" target="_blank">
+                                <FaInstagramSquare className={"footer-icon instagram"} />
+                                <FaInstagramSquare className={"footer-icon instagram-hover"} />
+                            </a>
+                        </div>
+                        <div><a href="https://www.pinterest.com" target="_blank"><FaPinterestSquare className={"footer-icon pinterest"} /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
