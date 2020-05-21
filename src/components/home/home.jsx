b358b6f6d2ce7c8d6f512f42634b5f4f5efdf554
saharovich <55681558+saharovich@users.noreply.github.com>
2020-05-21 11:13:28 +0300

    basic info line

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 // components
 import HomeSearch from './home_search';
 // import LineGallery from './';
-// import InfoLine from './';
+import InfoLine from './info_line';
 import Footer from '../footer/footer';
 
 const Home = ({type, title, description}) => {
+    const info = [{number: '1,290', description: 'Homes For Sale'}, {number: '843', description: 'Homes For Rent'}, {number: '945', description: 'Recently Sold'}, {number: '390', description: 'Recently Rented'}, {number: '594', description: 'New Estates'}]
+    
     return (
         <div id="home">
             <HomeSearch title={title} description={description} />
-            {/* <LineGallery/>
-            <InfoLine/> */}
+            {/* <LineGallery/> */}
+            <InfoLine info={info}/>
             <Footer/>
         </div>
     );
