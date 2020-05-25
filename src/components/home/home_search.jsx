0d2eba1f7c5f6f41c54183d2e2a46a1cfd6d24ad
saharovich <55681558+saharovich@users.noreply.github.com>
2020-05-25 14:46:34 +0300

    styling

--- src/components/home/home_search.jsx
+++ src/components/home/home_search.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomeSearch = ({ title, description }) => {
+    const sale_status = 'buy';
     return (
-        <div id="search">
-            <div className={'container'}>
+        <div id={"search"} className={'cover-photo'}>
+            <div className={'container content-wrapper'}>
                 <h1 className={'title'}>{title}</h1>
-                <h3 className={'description'}>{description}</h3>
-                <ul className={'row'}>
-                    <li className={'col-auto'}>
-                        <Link to='/buy'>Buy</Link>
+                <h3 className={'description d-sm-block'}>{description}</h3>
+                <ul className={'row sale-options-wrapper'}>
+                    <li className={`col-auto sale-options ${sale_status === "sale" && "list-chosed"}`}>
+                        <Link to='/buy' className={'bottom-line'}>Buy</Link>
                     </li>
-                    <li className={'col-auto'}>
-                        <Link to='/rent'>Rent</Link>
+                    <li className={`col-auto  sale-options ${sale_status === "rent" && "list-chosed"}`}>
+                        <Link to='/rent' className={'bottom-line'}>Rent</Link>
                     </li>
-                    <li className={'col-auto'}>
-                        <Link to='/sell'>Sell</Link>
+                    <li className={`col-auto  sale-options ${sale_status === "sell" && "list-chosed"}`}>
+                        <Link to='/sell' className={'bottom-line'}>Sell</Link>
                     </li>
                 </ul>
                 <div className="row">
                     <form className="col" action="search.html">
-                        <div className="row">
-                            <select className="col">
+                        <div className="row search-form">
+                            <select className="col select">
                                 <option>Chose Countries</option>
                                 {/* {
                                     countries.map(country => (
@@ -29,7 +30,7 @@ const HomeSearch = ({ title, description }) => {
                                     ))
                                 } */}
                             </select>
-                            <input className="col-auto" type="submit" name="submit" value="Search">
+                            <input className="col-auto submit" type="submit" name="submit" value="Search">
                                 {/* <Link to={{
                                     pathname: "/gallery",
                                     state: {
