ba1a2b6f557aab39764749de8b4f38b1daa1a25d
saharovich <55681558+saharovich@users.noreply.github.com>
2020-06-08 15:36:07 +0300

    prev and next button is connacted and work

--- src/components/singleApartment/nextOrPrevApartment.jsx
+++ src/components/singleApartment/nextOrPrevApartment.jsx
@@ -1,63 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-class NextOrPrevApartment extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            apartmentId: this.props.id,
-            // apartmentData: apartment,
-            // apartmentImage: apartment.main_image,
-            type: this.props.type,
-            price: false
-        };
-    }
-
-    render() {
-        const {apartmentId} =this.state;
-        return (
-            <a href={`/apartment/${apartmentId}`}>
-                <div
-                     style={{
-                         zIndex: 2,
-                         left: this.state.type === 'prev' && 0,
-                         right: this.state.type !== 'prev' && 0,
-                         bottom: "5vh",
-                         position: "fixed",
-                         backgroundSize: "cover",
-                         background: 'blue',
-                        //  backgroundImage: `url(/${this.state.apartmentImage})`
-                     }}>
-                    <div onMouseOver={() => {
-                        this.setState({price: true})
-                    }} onMouseLeave={() => {
-                        this.setState({price: false})
-                    }} className={"nextPrevApartmentPointer"}>
-                        {/* {this.state.type === "next"
-                            ? <div><span>Next</span> <i className={"fa fa-chevron-right"}/>{this.state.price &&
-                            <div>{this.state.apartmentData.price}</div>}</div>
-                            : <div><i className={"fa fa-chevron-left"}/> <span>Prev</span> {this.state.price &&
-                            <div>{this.state.apartmentData.price}</div>} </div>} */}
-                    </div>
+const NextOrPrevApartment = ({ apartment, type }) => {
+    const [showPrice, setShowPrice] = useState(false);
+    return (
+        <Link to={`/apartment/${apartment.id}`}>
+            <div
+                style={{
+                    zIndex: 2,
+                    left: type === 'prev' && 0,
+                    right: type !== 'prev' && 0,
+                    bottom: "5vh",
+                    position: "fixed",
+                    backgroundSize: "cover",
+                    backgroundImage: `url(/${apartment.main_image})`
+                }}>
+                <div onMouseOver={() => {
+                    setShowPrice(true)
+                }} onMouseLeave={() => {
+                    setShowPrice(false)
+                }} className={"nextPrevApartmentPointer"}>
+                    {type === "next"
+                        ? <div><span>Next</span> <i className={"fa fa-chevron-right"} />{showPrice &&
+                            <div>{apartment.price}</div>}</div>
+                        : <div><i className={"fa fa-chevron-left"} /> <span>Prev</span> {showPrice &&
+                            <div>{apartment.price}</div>} </div>}
                 </div>
-            </a>
-        );
-    }
-
-    static defaultProps = {
-        apartmentData: {}
-    }
+            </div>
+        </Link>
+    );
 }
 
-export default NextOrPrevApartment;
-
-// import React from 'react';
-
-// const NextOrPrevApartment = () => {
-//     return (
-//         <div>
-            
-//         </div>
-//     );
+// static defaultProps = {
+//     apartmentData: {}
+// }
 // };
 
-// export default NextOrPrevApartment;
+export default NextOrPrevApartment;
--- src/components/singleApartment/singleApartment.jsx
+++ src/components/singleApartment/singleApartment.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, withRouter } from 'react-router-dom';
 
 import Carousel from "./carousel";
-import NextOrPrevApartment from "./nextOrPrevApartment";
 import ApartmentDetails from "./apartmentDetails";
 import ApartmentMap from "./apartmentMap";
 
 import { apartments } from '../../app-data/apartment-data';
 import Footer from '../footer/footer';
+import NextOrPrevApartment from './nextOrPrevApartment';
+
+const searchPrevApartment = (curId) => {
+    let prevId = parseInt(curId);
+    let prev = {};
+    for (let i = 0; i < apartments.length; i++) {
+        if (--prevId >= 0) {
+            prev = apartments.find(apartment => parseInt(apartment.id) === prevId);
+            if (prev) {
+                return prev;
+            }
+        }
+    }
+}
+
+const searchNextApartment = (curId) => {
+    let nextId = parseInt(curId);
+    let next = {};
+    for (let i = 0; i < apartments.length; i++) {
+        if (++nextId >= 0) {
+            next = apartments.find(apartment => parseInt(apartment.id) === nextId);
+            if (next) {
+                return next;
+            }
+        }
+    }
+}
 
 const SingleApartment = () => {
     const { id } = useParams();
     const apartmentData = apartments.find(apartment => parseInt(apartment.id) === parseInt(id));
     const apartmentImages = apartmentData.images;
+
+    const prevApartment = searchPrevApartment(id);
+    const nextApartment = searchNextApartment(id);
     return (
         <div id={"apartment"}>
             <div className={"container"}>
@@ -21,6 +50,8 @@ const SingleApartment = () => {
                     <ApartmentDetails apartmentData={apartmentData} />
                     <ApartmentMap />
                 </div>
+                <NextOrPrevApartment type={"prev"} apartment={prevApartment} />
+                <NextOrPrevApartment type={"next"} apartment={nextApartment} />}
             </div>
             <Footer />
         </div>
