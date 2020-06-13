154945b26b9b7436cd8800962c3d4e5b0929e8c9
saharovich <55681558+saharovich@users.noreply.github.com>
2020-06-13 22:24:57 +0300

    button show more and show less is done

--- src/components/galleryAndSearch/gallery/gallery.jsx
+++ src/components/galleryAndSearch/gallery/gallery.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Card from "./card/card";
+import { useState } from "react";
 
 const Gallery = ({ items }) => {
+    const [paginationIndex, setPaginationIndex] = useState(6);
+
     return (
         <div id={"gallery"} className={"container-fluid"}>
             <h4 className={"mt-3"}>Real Estate & Homes For Sale</h4>
@@ -9,12 +12,36 @@ const Gallery = ({ items }) => {
             <div className={"row"}>
                 {items ? items.map((item, i) => {
                     return (
+                        i < paginationIndex &&
                         <Card data={item} key={i} />
                     )
                 }) :
                     <p>No apartments match for this search</p>
                 }
             </div>
+            {
+                items &&
+                <div className={'d-flex justify-content-around'}>
+                    {
+                        paginationIndex < items.length &&
+                        <button className={'buttonStyle'} onClick={() => setPaginationIndex(paginationIndex + 6)}>Show More</button>
+                    }
+                    {
+                        paginationIndex > 6 &&
+                        <button className={'buttonStyle'} onClick={() => setPaginationIndex(paginationIndex - 6)}>Show Less</button>
+                    }
+                </div>
+            }
+            {/* {items && paginationIndex < items.length &&
+                <div className={'display-center'}>
+                    <button className={'buttonStyle'} onClick={() => setPaginationIndex(paginationIndex + 6)}>Show More</button>
+                </div>
+            }
+            {items && paginationIndex > 6 &&
+                <div className={'display-center'}>
+                    <button className={'buttonStyle'} onClick={() => setPaginationIndex(paginationIndex - 6)}>Show Less</button>
+                </div>
+            } */}
         </div>
     )
 };
